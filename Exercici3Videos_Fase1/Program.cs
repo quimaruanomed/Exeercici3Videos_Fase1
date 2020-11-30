1092e4a98b292c0dd6d170f2053fb55fae0fd7c2
quimaruanomed <jruanomed@gmail.com>
2020-11-30 10:20:40 +0100

    first commit

---
+++ Exercici3Videos_Fase1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercici3Videos_Fase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
---
+++ Exercici3Videos_Fase1/clases/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercici3VideosFases_2.Clases
+{
+    public class User
+    {
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime DatRegister { get; set; }
+
+
+
+        public User()
+        {
+
+        }
+
+
+        public static List<User> uList = new List<User>
+        {
+           new User {Name="Joaquina", Surname="Ruano",Username="jruanomed",Password="jruanomed" },
+           new User {Name= "Javier", Surname= "Perez", Username= "jperez", Password= "jperez" },
+           new User {Name= "Admin", Surname= "Admin", Username= "admin", Password= "admin"}};
+    }
+
+
+
+
+
+
+}
+
